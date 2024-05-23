68922fc7adc328989327cd5ed3626cc94723929e
tech dor <tech.aharoni@gmail.com>
2024-05-23 23:47:42 +0300

    first deploy

--- function_app.py
+++ function_app.py
@@ -1,10 +1,39 @@
 import azure.functions as func
 import logging
+import os #in order to get parameters values from azure function app enviroment vartiable - sql password for example 
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient # in order to use azure container storage
+import io # in order to download pdf to memory and write into memory without disk permission needed 
+import json # in order to use json 
+import pyodbc #for sql connections 
+from azure.servicebus import ServiceBusClient, ServiceBusMessage # in order to use azure service bus 
+from openai import AzureOpenAI #for using openai services 
+from azure.data.tables import TableServiceClient, TableClient, UpdateMode # in order to use azure storage table  
+from azure.core.exceptions import ResourceExistsError, ResourceNotFoundError # in order to use azure storage table  exceptions 
+import csv #helping convert json to csv
+
+
+# Azure Blob Storage connection string
+connection_string_blob = os.environ.get('BlobStorageConnString')
+
+#Azure service bus connection string 
+connection_string_servicebus = os.environ.get('servicebusConnectionString')
+
+# Define connection details
+server = 'medicalanalysis-sqlserver.database.windows.net'
+database = 'medicalanalysis'
+username = os.environ.get('sql_username')
+password = os.environ.get('sql_password')
+driver= '{ODBC Driver 18 for SQL Server}'
 
 app = func.FunctionApp()
 
 @app.service_bus_queue_trigger(arg_name="azservicebus", queue_name="handleduplicatediagnosis",
                                connection="medicalanalysis_SERVICEBUS") 
 def handleDuplicateDiagnosis(azservicebus: func.ServiceBusMessage):
-    logging.info('Python ServiceBus Queue trigger processeds a message: %s',
-                azservicebus.get_body().decode('utf-8'))
+    message_data = azservicebus.get_body().decode('utf-8')
+    logging.info(f"Received messageesds: {message_data}")
+    message_data_dict = json.loads(message_data)
+    caseid = message_data_dict['caseid']
+    clinicArea = message_data_dict['clinicArea']
+    sourceTable = message_data_dict['sourceTable']
+    logging.info(f"event data:caseid:{caseid},clinicArea:{clinicArea},sourceTable:{sourceTable}")
